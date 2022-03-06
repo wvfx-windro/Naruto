1d26e9694b395078bc993a8957f29d20b8727c0c
wvfx-windro <100778740+wvfx-windro@users.noreply.github.com>
2022-03-06 22:44:46 +0530

    Update jasil.js

--- plugins/jasil.js
+++ plugins/jasil.js
@@ -11,18 +11,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bᴏᴛ Nᴀᴍᴇ 🇦 🇲 🇷 🇺   🇸 🇪 🇷  🇧 🇴 🇹 *
 
-*Cʀᴇᴀᴛᴇʀ number : wa.me/917025631103?text=Hi%20ᴀᴍʀᴜ%20bro.%20*
-
-
- *Aᴍʀᴜ sᴇʀ Cʜᴀɴᴀʟ : https://youtu.be/x47g62HcsWc😁*
+ *Cʀᴇᴀᴛᴇʀ number : wa.me/918606413490?text=Hi%20Windro%20bro.%20*
      
- *Iɴsᴛᴀɢʀᴀᴍ ɪᴅ: https://instagram.com/its_me_dron?utm_medium=copy_link*
+ *Iɴsᴛᴀɢʀᴀᴍ ɪᴅ: https://instagram.com/ff.wvfx*
 
- *Aᴍʀᴜ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/DnHJu25Ccss7zn72nPhL8z*
+ *ɴᴀʀᴜᴛᴏ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/FPCaXl55jkYK3PgTDtdvAV*
 
- *Gɪᴛ Lɪɴᴋ : https://github.com/AMRUSIR/AMRU-SER*
+ *Gɪᴛ Lɪɴᴋ : https://github.com/wvfx-windro/Naruto*
 
- *Aᴍʀᴜ-sᴇʀ-ʙᴏᴛꫂ⁩..♡︎*
+ *Nᴀʀᴜᴛᴏꫂ⁩..♡︎*
 `}) 
 
 }));
